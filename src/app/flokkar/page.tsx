0881df767053725ea007aee5842881c079556e97
Ásdís Valtýsdóttir <asdisval@gmail.com>
2025-03-20 15:49:55 +0000

    intro komid

--- src/app/flokkar/page.tsx
+++ src/app/flokkar/page.tsx
@@ -1,11 +1,11 @@
-import Categories from '@/components/Categories/Categories';
+import styles from './page.module.css';
 import Navigation from '@/components/Navigation/Navigation';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <Navigation />
-      <Categories title="Allir flokkar" />
+      forsíða!
     </div>
   );
 }
